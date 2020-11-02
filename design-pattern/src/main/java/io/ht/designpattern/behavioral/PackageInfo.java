dcac3a462c86f11ca9a181eda9b90544dfee0595
lihuatai <huatailee@sina.com>
2020-11-02 17:09:58 +0800

    更新文件名

---
+++ design-pattern/src/main/java/io/ht/designpattern/behavioral/PackageInfo.java
@@ -0,0 +1,7 @@
+package io.ht.designpattern.behavioral;
+
+public class PackageInfo {
+    /**
+     * 行为模式：负责对象间的高效沟通和职责委派
+     */
+}
---
+++ design-pattern/src/main/java/io/ht/designpattern/creational/factory/package-info.java
@@ -0,0 +1,1 @@
+package io.ht.designpattern.creational.factory;
